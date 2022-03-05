b6d5b763c99c8e4e27a16d7254822ae4af62b6b5
Kailash Babu <iamkailash1995@gmail.com>
2022-03-05 10:27:29 +0530

    my first commit reactwebsite

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import web from '../src/images/hero-img.png'
+import { NavLink } from 'react-router-dom'
+import Commom from './Commom'
+
+const About = () => {
+    return (
+        <>
+        <Commom name="Welcome to About Page" imgsrc={web} visit="/contact" btname="Contact Now"/ >
+        </>
+    )
+}
+
+export default About;
